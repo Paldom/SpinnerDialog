5c8b2d4523ec096646cc50058e9abd3595586550
PaoloMessina <phemt.latd@gmail.com>
2015-11-19 14:50:38 +0100

    commit spinner
    spinner style

--- src/windows/SpinnerDialogProxy.js
+++ src/windows/SpinnerDialogProxy.js
@@ -1,6 +1,15 @@
 cordova.commandProxy.add("SpinnerDialog", {
 	show: function (successCallback, errorCallback, params) {
-		var msgBox = new Windows.UI.Popups.MessageDialog("prova");
-		msgBox.showAsync();
+		//var msgBox = new Windows.UI.Popups.MessageDialog("prova");
+		//msgBox.showAsync();
+	    var element = element || document.createElement("div");
+	    //element.style = "width:100%; height:100%; background-color: #000000";
+	    element.style.width = "100%";
+	    element.style.height = "100%";
+	    element.style.position = "relative";
+	    element.style.opacity = .5; //For real browsers;
+	    element.style.filter = "alpha(opacity=50)"; //For IE;
+	    element.style.background = "#000000";
+	    element.style.zIndex = 999999;
 	}
 });
