cfd175dfa435707eef11b9271bdd273723b7eb4b
PaoloMessina <phemt.latd@gmail.com>
2015-11-19 15:40:58 +0100

    commit show/hide function
    commit show/hide function

--- src/windows/SpinnerDialogProxy.js
+++ src/windows/SpinnerDialogProxy.js
@@ -1,19 +1,37 @@
 cordova.commandProxy.add("SpinnerDialog", {
 	show: function (successCallback, errorCallback, params) {
-		//var msgBox = new Windows.UI.Popups.MessageDialog("prova");
-		//msgBox.showAsync();
-	    var element = element || document.createElement("div");
-	    //element.style = "width:100%; height:100%; background-color: #000000";
-	    element.style.width = "100%";
-	    element.style.height = "100%";
-	    element.style.position = "relative";
-	    element.style.opacity = .5; //For real browsers;
-	    element.style.filter = "alpha(opacity=50)"; //For IE;
-	    element.style.background = "#000000";
-	    element.style.zIndex = "999999";
-		
-		element.innerHTML = "<progress class=\"win-ring\" style=\"position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%);\"></progress>";
-		
-		document.body.appendChild(element);
+		if(!document.getElementById("SpinnerDialogPlugin"))
+			addSpinner();
+		showSpinner();
+	},
+	hide: function (successCallback, errorCallback, params) {
+		hideSpinner();
 	}
 });
+
+function showSpinner(){
+	var element = document.getElementById("SpinnerDialogPlugin");
+	if(element) element.style.display = "block";
+};
+
+function hideSpinner(){
+	var element = document.getElementById("SpinnerDialogPlugin");
+	if(element) element.style.display = "none";
+};
+
+function addSpinner() {
+	var element = element || document.createElement("div");
+	element.setAttribute("id", "SpinnerDialogPlugin");
+	element.style.width = "100%";
+	element.style.height = "100%";
+	element.style.position = "relative";
+	element.style.opacity = .5; //For real browsers;
+	element.style.filter = "alpha(opacity=50)"; //For IE;
+	element.style.background = "#000000";
+	element.style.zIndex = "999999";
+	element.style.display = "none";
+	
+	element.innerHTML = "<progress class=\"win-ring\" style=\"position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%);\"></progress>";
+	
+	document.body.appendChild(element);
+};
