d4a6a20117d24fd2cebd9a953f3e890f8283c963
PaoloMessina <phemt.latd@gmail.com>
2015-11-19 12:47:19 +0100

    bugfix proxy
    bugfix proxy

--- src/windows/SpinnerDialogProxy.js
+++ src/windows/SpinnerDialogProxy.js
@@ -1,5 +1,5 @@
 cordova.commandProxy.add("SpinnerDialog", {
-	createEventWithOptions: function (successCallback, errorCallback, params) {
+	show: function (successCallback, errorCallback, params) {
 		var msgBox = new Windows.UI.Popups.MessageDialog("prova");
 		msgBox.showAsync();
 	}
