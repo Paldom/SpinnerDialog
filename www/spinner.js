386df69db7bab7282a95a259a6df4664dbd7a709
Cameron Bates <darksonic8000@gmail.com>
2016-02-01 12:18:18 -0500

    Add boolean to prevent multiple masks
    If a mask is already visible there's no need to display another

--- www/spinner.js
+++ www/spinner.js
@@ -2,16 +2,22 @@ var exec = require('cordova/exec');
 
 module.exports = {
 
-	show : function(title, message, cancelCallback) {
-        if (cancelCallback == true && typeof cancelCallback !== "function") {
+	show: function(title, message, cancelCallback) {
+        if (cancelCallback === true && typeof cancelCallback !== "function") {
             cancelCallback = function () {};  
         }
-        cordova.exec(cancelCallback, null, 'SpinnerDialog', 'show', [ title, message, !!cancelCallback ]);
+		if (!this.masked) {
+			this.masked = true;
+			cordova.exec(cancelCallback, null, 'SpinnerDialog', 'show', [ title, message, !!cancelCallback ]);
+		}
     },
 
-
-    hide : function(success, fail) {
+    hide: function(success, fail) {
+		this.masked = false;
         cordova.exec(success, fail, 'SpinnerDialog', 'hide', [ "","" ]);
-    }
-
+    },
+	
+	isMasked: function() {
+		return this.masked ? true : false;
+	}
 };
